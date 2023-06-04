71e76a96a944758feb2220a8e838de7f3920c3b7
Cactus <ysjungman@gmail.com>
2023-06-04 14:27:27 +0900

    358_Meetup_출력

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A movei 1',
+    image: 'https://i1.sndcdn.com/artworks-2gFFevu0kuLUtbyP-TFd6Rg-t500x500.jpg',
+    adress: 'korea',
+    description: 'eazy day'
+  },
+  {
+    id: 'm2',
+    title: 'A drama 1',
+    image: 'https://file1.bobaedream.co.kr/multi_image/strange/2017/08/09/13/Dg0598a92bd65872.jpg',
+    adress: 'japan',
+    description: 'lucky day'
+  },
+];
+
+function HomePage() {
+  return (
+    <MeetupList meetups={DUMMY_MEETUPS} />
+  );
+}
+
+export default HomePage;
