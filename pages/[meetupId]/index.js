82d8ba74033fee793793dbd09122b5bd1889ecb3
Cactus <ysjungman@gmail.com>
2023-06-05 10:00:59 +0900

    362_customComponent_cssModule

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://i1.sndcdn.com/artworks-2gFFevu0kuLUtbyP-TFd6Rg-t500x500.jpg' alt='bobman'
+      title="kang-ho"
+      address="moive"
+      description="Memories of Murder"
+    />
+  );
+}
+
+export default MeetupDetails;
