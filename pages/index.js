c587683ab58ce3a459a06bb054f659feeed30f21
Cactus <ysjungman@gmail.com>
2023-06-05 13:04:28 +0900

    366_getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,22 @@ function HomePage(props) {
   );
 }
 
+// getServerSideProps : 서버요청받은 시점에서 데이터를 받아와 동적 생성, 
+//                      서버요청 받을때까지 대기상태
+// Pre-generated : 정적 콘텐츠를 가리키는 용어, 페이지를 미리 생성하여 정적인 HTML 파일로 저장하는 기능
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+// getStaticProps : 빌드시점에서 페이지 정적 데이터 가져와 렌더링
+// revalidate : next.js 정적생성기능 관련 옵션, 페이지를 얼마나 자주 재생성하는지 지정하여 사용됩니다.
 export async function getStaticProps() {
   return {
     props: {
@@ -32,6 +48,6 @@ export async function getStaticProps() {
   };
 }
 
-// revalidate : next.js 정적생성기능 관련 옵션, 페이지를 얼마나 자주 재생성하는지 지정하여 사용됩니다.
+
 
 export default HomePage;
