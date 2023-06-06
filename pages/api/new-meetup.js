49fcceab489582bc6d3fdb1e8357766ae9e5f667
Cactus <ysjungman@gmail.com>
2023-06-07 03:04:32 +0900

    370_MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,22 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect('mongodb+srv://ysjungman:881202@cluster0.lfkfhzv.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db = client.db();
 
+    const meetupsCollection = db.collection('meetups');
 
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
-};
+}
 
 export default handler;
