9efe0a0008d01684555fe0e32727c0de9846a761
Cactus <ysjungman@gmail.com>
2023-06-08 07:00:57 +0900

    372_DB_데이터_가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A movei 1',
-    image: 'https://i1.sndcdn.com/artworks-2gFFevu0kuLUtbyP-TFd6Rg-t500x500.jpg',
-    adress: 'korea',
-    description: 'eazy day'
-  },
-  {
-    id: 'm2',
-    title: 'A drama 1',
-    image: 'https://file1.bobaedream.co.kr/multi_image/strange/2017/08/09/13/Dg0598a92bd65872.jpg',
-    adress: 'japan',
-    description: 'lucky day'
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: 'm1',
+//     title: 'A movei 1',
+//     image: 'https://i1.sndcdn.com/artworks-2gFFevu0kuLUtbyP-TFd6Rg-t500x500.jpg',
+//     adress: 'korea',
+//     description: 'eazy day'
+//   },
+//   {
+//     id: 'm2',
+//     title: 'A drama 1',
+//     image: 'https://file1.bobaedream.co.kr/multi_image/strange/2017/08/09/13/Dg0598a92bd65872.jpg',
+//     adress: 'japan',
+//     description: 'lucky day'
+//   },
+// ];
 
 function HomePage(props) {
   return (
@@ -40,9 +42,24 @@ function HomePage(props) {
 // getStaticProps : 빌드시점에서 페이지 정적 데이터 가져와 렌더링
 // revalidate : next.js 정적생성기능 관련 옵션, 페이지를 얼마나 자주 재생성하는지 지정하여 사용됩니다.
 export async function getStaticProps() {
+  // fetch('/api/meetups');
+  const client = await MongoClient.connect('mongodb+srv://ysjungman:881202@cluster0.lfkfhzv.mongodb.net/meetups?retryWrites=true&w=majority');
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      }))
     },
     revalidate: 1
   };
