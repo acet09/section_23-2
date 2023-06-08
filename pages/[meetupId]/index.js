b4420794c14d70a614d3e5cdaa67c40e9c5a3588
Cactus <ysjungman@gmail.com>
2023-06-08 11:53:35 +0900

    373_Meetup_세부정보_데이터

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,49 +1,66 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image='https://i1.sndcdn.com/artworks-2gFFevu0kuLUtbyP-TFd6Rg-t500x500.jpg'
-      alt='bobman'
-      title="kang-ho"
-      address="moive"
-      description="Memories of Murder"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 // getStaticPaths : 정적 생성(static generation)을 사용하는 페이지에서 동적 경로를 생성하는 데 사용됩니다. 이 함수는 동적 경로의 목록을 반환하고, 각 경로에 대한 정적 페이지를 미리 생성합니다.
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://ysjungman:881202@cluster0.lfkfhzv.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ]
-  }
+    paths: meetups.map(meetup => ({
+      params: { meetupId: meetup._id.toString() }
+    })),
+  };
 }
 
 export async function getStaticProps(context) {
+  // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  const client = await MongoClient.connect(
+    'mongodb+srv://ysjungman:881202@cluster0.lfkfhzv.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  console.log(selectedMeetup);
+
+  client.close();
 
   return {
     props: {
-      meetupDate: {
-        image: 'https://i1.sndcdn.com/artworks-2gFFevu0kuLUtbyP-TFd6Rg-t500x500.jpg',
-        id: meetupId,
-        title: 'kang-ho',
-        address: 'moive',
-        description: 'Memories of Murder'
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
